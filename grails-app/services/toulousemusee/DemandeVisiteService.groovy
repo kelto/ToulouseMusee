913ef67013f4e794b12670e0ac6aa979c2b2f900
Ludovic <ludo.castelli@hotmail.fr>
2015-04-26 17:37:28 +0200

    Probleme sur demandeVisite.save !!!

--- grails-app/services/toulousemusee/DemandeVisiteService.groovy
+++ grails-app/services/toulousemusee/DemandeVisiteService.groovy
@@ -4,27 +4,16 @@ import grails.transaction.Transactional
 
 @Transactional
 class DemandeVisiteService {
-    private static final int NB_CHAR_CODE = 4;
 
-    def addVisite(Musee m,Date debut, Date fin, int nb) {
-        println "********************************************"
-        println "begin"
-        Random random = new Random();
-        println "random created"
-        String c = "";
-        for(int i = 0; i < NB_CHAR_CODE; i++) {
-            c += random.nextInt(10).toString();
-        }
-        println "code created"
-        DemandeVisite visite = new DemandeVisite(code: c, dateDebutPeriode: debut, dateFinPeriode: fin, nbPersonnes: nb, statut: DemandeVisite.TO_BE_TREATED)
-        println "visite created"
-        DemandeVisiteMusee visiteMusee = new DemandeVisiteMusee(dateDemande: new Date(),musee: m, demandeVisiste: visite)
-        println "visite musee created"
-        visite.save(flush: true)
-        println "visite saved"
-        visiteMusee.save(flush: true)
-        println "visite musee saved"
-        println "success"
+    MuseeService museeService
+    def addVisite(DemandeVisite demandeVisite, DemandeVisiteMusee demandeVisiteMusee, Musee musee) {
+        musee.id ? null : museeService.insertOrUpdateMusee(musee,musee.adresse,musee.gestionnaire)
+        if(demandeVisite.id==null){ if(demandeVisite.save(flush: true)==null)println("error demandeVisite save")}
 
+        demandeVisiteMusee.musee=musee
+        demandeVisiteMusee.demandeVisite=demandeVisite
+
+        demandeVisiteMusee.save(flush:true)
+        demandeVisite
     }
 }
