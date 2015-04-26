56ad474c71039daa39252135c6d69034f2275411
Ludovic <ludo.castelli@hotmail.fr>
2015-04-26 17:35:39 +0200

    Edit visite()

--- grails-app/controllers/toulousemusee/VisiteController.groovy
+++ grails-app/controllers/toulousemusee/VisiteController.groovy
@@ -2,15 +2,28 @@ package toulousemusee
 
 class VisiteController {
 
-    MuseeService museeService
-    MuseesFavorisService museesFavorisService
     DemandeVisiteService demandeVisiteService
     def index() {
-        [favoris: museesFavorisService.getFavorites(session)]
     }
 
     def visite() {
-        def debut,fin
-        demandeVisiteService.addVisite(Musee.findById(params.int("id")),)
+        def debut= params.debut
+        def fin = params.fin
+        def nb = params.nb
+
+        Musee musee=Musee.findByNom(session.getAttribute("nomMusee"))
+        DemandeVisiteMusee demandeVisiteMusee=new DemandeVisiteMusee(dateDemande: new Date())
+        String code=session.getId() + new GregorianCalendar().getTimeInMillis().toString()
+        println(debut)
+        println(fin)
+        println(nb)
+        println(code)
+        println(musee.nom)
+        DemandeVisite demandeVisite=new DemandeVisite(dateDebutPeriode: debut, dateFinPeriode: fin, nbPersonnes: nb, statut: DemandeVisite.TO_BE_TREATED, code: code)
+
+        demandeVisiteService.addVisite(demandeVisite,demandeVisiteMusee,musee)
+        session.setAttribute("code",code)
+
+        render(view : "resultatDemande")
     }
 }
